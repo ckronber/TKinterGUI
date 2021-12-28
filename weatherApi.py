964046f328c83739435d90faab3f311a7297ab20
Chris Kronberg <ckronber@gmail.com>
2021-12-28 00:02:53 -0500

    updated weather app that gets api data

--- weatherApi.py
+++ weatherApi.py
@@ -4,18 +4,42 @@ from tkinter import filedialog,messagebox, filedialog
 #from PIL import ImageTk, Image
 import sqlite3
 import json,requests,sqlite3
-
 from requests import api
 
 root = Tk()
 root.title("Address Information Database")
 root.geometry("400x350")
 
-try:
-    api_request = requests.get("https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=46205&distance=25&API_KEY=D086A2F4-9F3E-4AC4-8491-BE808D183182")
-    api = json.loads(api_request.content)
-except Exception as e:
-    api = "Error: "+ str(e)
+zip_label = Label(root,text = "Enter Zip Code")
+zip_label.pack()
+my_zip = Entry(root,width = 20,borderwidth=2)
+my_zip.pack()
 
-my_label = Label(root, text=api[0]).pack()
+global myFrame
+
+
+def get_weather_data():
+    myFrame = Frame(root)
+    myFrame.pack()
+
+    try:
+        api_request = requests.get("https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode="+str(my_zip.get())+"&distance=25&API_KEY=D086A2F4-9F3E-4AC4-8491-BE808D183182")
+        api = json.loads(api_request.content)
+    except Exception as e:
+        api = "Error: "+ str(e)
+
+    myText = []
+    my_label = Label(myFrame,text=api)
+    my_label.pack()
+    locText = "Location: "+ api[0]['ReportingArea'] + " |  Date: " + api[0]['DateObserved']
+    my_label1 = Label(root, text=locText).pack()
+
+    for num in range(len(api)):
+        myText.append(api[num]['ParameterName'] + ": " +api[num]['Category']['Name'] + " | AQI: " + str(api[num]['AQI']))
+        my_label = Label(root, text=myText[num]).pack()
+
+    return my_label
+
+zip_button = Button(root,text = "Submit", command = get_weather_data).pack()
+
 root.mainloop()
